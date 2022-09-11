f3ffa8a8ad883b167507709e6dd68f6d36204aa7
Augustar Banafor <augustarbanafor@gmail.com>
2022-09-11 10:07:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
-#include <stdio.h>                                                                                                                                              
+#include <stdio.h>
 /**
- *main - main block
- *Return: 0
- **/                                                                                                                                                            
-
-int main(void)                                                                                                                                                  
-
-{                                                                                                                                                               
-
-	printf("with proper grammar, but the outcome is a piece of art,");   
-	return (0);
-
-}  
+*main-main block
+*Return: 0
+**/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
