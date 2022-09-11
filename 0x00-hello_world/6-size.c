d9416cd46c235db5eea15233045e060fc07bb035
Augustar Banafor <augustarbanafor@gmail.com>
2022-09-11 13:13:01 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - print the string in the put function
+*
+*Return: 0
+**/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %1d byte(s)\n", sizeof(c));
+	printf("Size of an int: %1d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
+	printf("Size of long long int: %1d byte(s)\n", sizeof(lli);
+	printf("Size of a float: %1d byte(s)\n", sizeof(f));
+	return (0);
+}
